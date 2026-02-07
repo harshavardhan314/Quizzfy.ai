479ba6c366bc8f965a1c5a71b26aee8034d6fcbf
harshavardhan314 <hharshavardhan974@gmail.com>
2026-02-07 21:46:39 +0530

    updated the controllers

--- backend/Routes/quizRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const quizController = require('../controllers/Quizcontroller');
-
-// Define specific routes
-router.post('/generate-quiz', quizController.generateQuiz);
-router.post('/generate-feedback', quizController.generateFeedback);
-
-module.exports = router;
